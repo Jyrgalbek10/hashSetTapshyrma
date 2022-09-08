1398af07445ccbc59f4d41119b36bf0bfb461eb6
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-09-08 21:09:29 +0300

    has-sel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
 public class Main {
+    public static final Random ran = new Random();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> list = new ArrayList<>();
+        int san = 0;
+        for (int i = 0; i < 400; i++) {
+            int san1 = ran.nextInt(0, 20);
+           list.add(san1);
+            san += san1;
+            System.out.println(list + " + " + san + " ="+list.size());
+
+            HashSet<Integer> hashSet = new HashSet<>(list);
+                System.out.println(hashSet);
+            System.out.println("");
+            }
+        }
+
     }
-}
