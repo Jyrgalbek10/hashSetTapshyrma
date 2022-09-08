c19ec3767f81d2047d4bb3f2af6d7c79804dd451
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-09-08 21:33:16 +0300

    sal-ket

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,21 @@ public class Main {
         ArrayList<Integer> list = new ArrayList<>();
         int san = 0;
         for (int i = 0; i < 400; i++) {
-            int san1 = ran.nextInt(0, 20);
+            int san1 =ran.nextInt(0, 20);
            list.add(san1);
             san += san1;
-            System.out.println(list + " + " + san + " ="+list.size());
-
-            HashSet<Integer> hashSet = new HashSet<>(list);
-                System.out.println(hashSet);
-            System.out.println("");
-            }
         }
+            System.out.println( list.size()+" + "+list);
+            System.out.println("="+san);
 
-    }
+        HashSet<Integer> hashSet = new HashSet<>(list);
+        int san2 = 0;
+        for (int i = 0; i < hashSet.size(); i++) {
+            san2+=i;
+            System.out.print(san2+" ");
+        }
+        }
+}
+
+
+
