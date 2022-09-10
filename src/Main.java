e3ccb7eac1687608d2660149d932196849b78050
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-09-10 15:02:50 +0300

    sal-ket

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,18 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        int san = 0;
-        for (int i = 0; i < 400; i++) {
-            int san1 =ran.nextInt(0, 20);
-           list.add(san1);
-            san += san1;
-        }
-            System.out.println( list.size()+" + "+list);
-            System.out.println("="+san);
 
-        HashSet<Integer> hashSet = new HashSet<>(list);
-        int san2 = 0;
-        for (int i = 0; i < hashSet.size(); i++) {
-            san2+=i;
-            System.out.print(san2+" ");
+        for (int i = 0; i < 10; i++) {
+            list.add( ran.nextInt(0, 2));
+            Collections.sort(list);
+           System.out.println(list);
+
+        }
+        System.out.println(" ");
+        LinkedList<Integer> linkedList = new LinkedList<>(list);
+        for (int i = 0; i < linkedList.size(); i++) {;
+            Collections.sort(linkedList);
+            System.out.println(linkedList);
         }
         }
 }
